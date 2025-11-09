74d41537a0385b5060f269ebbaf19983026ccd43
HC Torres <hctorres02@gmail.com>
2025-11-09 19:37:14 -0300

    Route: prefix

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => 'Home')->name('home');
+
+Route::prefix('/admin')->group(function () {
+    Route::get('/', fn () => 'Admin home')->name('admin.home');
+});
