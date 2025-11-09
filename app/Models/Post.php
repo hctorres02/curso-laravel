c219bb28fb769c306f924446b96d4e0d9cb03840
HC Torres <hctorres02@gmail.com>
2025-11-09 19:37:12 -0300

    Eloquent: fillable

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'author_id',
+        'category_id',
+        'cover_id',
+        'title',
+        'slug',
+        'headline',
+        'body',
+        'status',
+    ];
 }
