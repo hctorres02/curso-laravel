c24386daf2eb21c455a9018d32e932602cb77de8
HC Torres <hctorres02@gmail.com>
2025-11-09 19:37:15 -0300

    Controller: resource

---
+++ app/Http/Controllers/Admin/MediaController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Media;
+use Illuminate\Http\Request;
+
+class MediaController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Media $media)
+    {
+        //
+    }
+
+    public function edit(Media $media)
+    {
+        //
+    }
+
+    public function update(Request $request, Media $media)
+    {
+        return to_route('admin.medias.edit', $media);
+    }
+
+    public function destroy(Media $media)
+    {
+        return to_route('admin.media.index');
+    }
+}
---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Post $post)
+    {
+        return to_route('blog.post', [$post->category, $post]);
+    }
+
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        return to_route('admin.posts.edit', $post);
+    }
+
+    public function destroy(Post $post)
+    {
+        return to_route('admin.posts.edit', $post);
+    }
+}
