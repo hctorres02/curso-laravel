a4b0ee90603f7edec20d7ca16421dd001156e0ab
HC Torres <hctorres02@gmail.com>
2025-11-09 19:37:12 -0300

    Eloquent: accessor/mutator

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,12 +3,16 @@
 namespace App\Models;
 
 use App\Enums\PostStatus;
+use App\Traits\TimestampsFormatter;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
+    use TimestampsFormatter;
+
     protected $fillable = [
         'author_id',
         'category_id',
@@ -25,6 +29,13 @@ class Post extends Model
         return [
             'status' => PostStatus::class,
         ];
+    }
+
+    public function bodyDecoded(): Attribute
+    {
+        return Attribute::make(
+            get: fn (mixed $value, array $attributes) => decodeMarkdown($attributes['body'])
+        );
     }
 
     public function author(): BelongsTo
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use App\Traits\TimestampsFormatter;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Model
 {
+    use TimestampsFormatter;
+
     protected $fillable = [
         'avatar_id',
         'name',
