b68890defc7efdf091c4f52964ba56cb510dafd7
HC Torres <hctorres02@gmail.com>
2025-11-12 14:39:12 -0300

    View: stack

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf_token" content="{{ csrf_token() }}">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @stack('head')
         <title>@yield('title')</title>
     </head>
 
     <body>
         @yield('body')
+        @stack('body-end')
     </body>
 
 </html>
