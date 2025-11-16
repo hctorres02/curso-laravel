e8da284bd6cde629b288534d9e89ded74f5dcb6f
HC Torres <hctorres02@gmail.com>
2025-11-16 14:40:07 -0300

    Route: parameters

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,8 @@ Route::get('/', fn () => 'Home')->name('home');
 Route::prefix('/admin')->group(function () {
     Route::get('/', fn () => 'Admin home')->name('admin.home');
 });
+
+Route::prefix('/{category}')->group(function () {
+    Route::get('/', fn ($category) => $category)->name('blog.category');
+    Route::get('/{post}', fn ($category, $post) => "{$category}: {$post}")->name('blog.post');
+});
