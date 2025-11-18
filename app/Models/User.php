5d22a9d7a5686fd3a6c046f8112a84a43066084d
HC Torres <hctorres02@gmail.com>
2025-11-18 18:14:30 -0300

    Authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use App\Traits\TimestampsFormatter;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
     use HasFactory;
     use TimestampsFormatter;
